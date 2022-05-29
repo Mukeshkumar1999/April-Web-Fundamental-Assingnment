8a6949f8855ebddf1892baba0de89dff10d380e5
Mukeshkumar1999 <92779777+Mukeshkumar1999@users.noreply.github.com>
2022-05-29 15:15:00 +0530

    Add files via upload

---
+++ Test-6/var-let-const.js
@@ -0,0 +1,56 @@
+// Question 1:-
+//  Answer:-
+//              differnce b/w var,let, const
+
+//              var:-  var variables can be updated and re-declared within its scope;
+
+//              let:-  let variables can be updated but not re-declared;
+
+//            const:-  const variables can neither be updated nor re-declared.
+
+
+//            for Example:-
+
+
+var city = "Lucknow"
+console.log(city)
+
+var city = "Gorakhpur"
+console.log(city)
+
+var capital = "Lucknow"
+console.log(capital)
+
+city = "punjab"
+console.log(city)
+
+
+
+// for  let
+
+
+let city = "Lucknow"
+console.log(city)
+
+
+
+let city = "Gorakhpur"
+console.log(city)
+
+city = "gorakhpur"
+console.log(city)
+
+
+
+// for   coonst 
+
+
+const city;
+city = "lucknow"
+console.log(city)
+
+const city = "lucknow"
+console.log(city)
+
+city = "ppunjab"
+console.log(city)
